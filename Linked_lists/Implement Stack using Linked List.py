867e505e88eeca96d3633173a5d9daf217f9a479
soundarzozm <soundar.murugan1@learner.manipal.edu>
2020-11-27 21:38:26 +0530

    Add file

---
+++ Linked_lists/Implement Stack using Linked List.py
@@ -0,0 +1,48 @@
+# Class to represent a node
+class StackNode:
+
+	# Constructor to initialize a node
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+class Stack:
+    
+    def __init__(self):
+        self.root = None
+        
+    # The method push to push element into the stack
+    def push(self, data):
+        newNode = StackNode(data)
+        newNode.next = self.root
+        self.root = newNode
+        
+    # The method pop which return the element popped out of the stack
+    def pop(self):
+        if(self.root == None):
+            return -1
+        temp = self.root
+        self.root = self.root.next
+        return temp.data
+
+#{ 
+#  Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = Stack()
+        q = int(input())
+        q1 = list(map(int, input().split()))
+        i = 0
+        while(i < len(q1)):
+            if(q1[i] == 1):
+                s.push(q1[i + 1])
+                i = i + 2
+            elif(q1[i] == 2):
+                print(s.pop(), end=" ")
+                i = i + 1
+            elif(s.isEmpty()):
+                print(-1)
+        print()
+
+# } Driver Code Ends
